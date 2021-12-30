094cc6da030481faa5153ab05d2293ec81305e7d
mifune96 <imrank.sensea@gmail.com>
2021-12-30 10:19:36 +0700

    add KTX

--- app/src/main/java/com/tomuchcoffee/restaurantreview/MainActivity.kt
+++ app/src/main/java/com/tomuchcoffee/restaurantreview/MainActivity.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethod
 import android.view.inputmethod.InputMethodManager
+import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -23,6 +24,7 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val mainViewModel by viewModels<MainViewModel>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +34,6 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        val mainViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
         mainViewModel.restaurant.observe(this, { restaurant ->
             setRestaurantData(restaurant)
         })
